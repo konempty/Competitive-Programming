0dce7e2cf049a92ea1a7ad9a5fc6b83f0561f5ba
cupjoo <cupjoo@naver.com>
2019-05-05 13:16:07 +0000

    43일 차

--- Junyoung/ACM-ICPC/파일_합치기.cpp
+++ Junyoung/ACM-ICPC/파일_합치기.cpp
@@ -1,42 +1,34 @@
 #include <cstdio>
+#include <cstring>
+#include <string>
 #include <algorithm>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-#define FOR(i,s,n) for(int i = s; i < n; i++)
-const int MAX_K = 500 + 1;
-const int MAX_VAL = 2e9;
-
-int cost[MAX_K];
-int sum[MAX_K];			// [1,K] 구간의 합 
-int dp[MAX_K][MAX_K];	// [a,b] 구간의 LCS
-
-int dnc(int s, int e) {
-	if (dp[s][e] < MAX_VAL)	return dp[s][e];
-	if (s == e)		return dp[s][e] = 0;
-	if (s+1 == e)	return dp[s][e] = cost[s] + cost[e];
-
-	FOR(m, s, e) {
-		dp[s][e] = min(dp[s][e], dnc(s, m) + dnc(m+1, e));
-	}
-	return dp[s][e] += sum[e]-sum[s-1];	// 마지막 합 연산
-}
-
+#define FOR(i,s,e) for(int i = s; i < e; i++)
+#define FORr(i,e,s) for(int i = e; i >= s; i--)
+#define ALL(x) (x).begin(), (x).end()
+typedef long long ll;
+ 
 int main() {
-	int T;
-
-	scanf("%d", &T);
-	FOR(t, 0, T){
-		int K;
+	int N;
+	
+	scanf("%d", &N);
+	
+	int cnt = -1;
+	if(N%2 && N%5){
+		cnt = 1;
+		int one = 1;
 		
-		scanf("%d", &K);
-		FOR(i, 1, K+1) {
-			scanf("%d", &cost[i]);
-			sum[i] = sum[i-1] + cost[i];
+		while(one %N){
+			one %= N;
+			one = one*10 +1;
+			cnt++;
 		}
-		FOR(i, 1, K+1)	FOR(j, 1, K+1) {
-			dp[i][j] = MAX_VAL;
-		}
-		printf("%d\n", dnc(1, K));
 	}
-	return 0;
+	printf("%d\n", cnt);
+	
+    return 0;
 }
