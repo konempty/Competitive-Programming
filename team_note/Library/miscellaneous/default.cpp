4640d9c1c4c3aa53bb16ef1eb406356b13b4dba2
cupjoo <cupjoo@naver.com>
2019-12-05 13:18:05 +0000

    매크로 수정

--- team_note/Library/miscellaneous/default.cpp
+++ team_note/Library/miscellaneous/default.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 #define FOR(i,s,e) for(int i = s; i < e; i++)
 #define FORr(i,e,s) for(int i = e; i >= s; i--)
-#define FORo(i,o) for(int i = 0, e = o.size(); i < e; i++)
+#define FORo(i,e,o) for(int i = 0, e = o.size(); i < e; i++)
 #define ALL(x) (x).begin(), (x).end()
 typedef long long ll;
 typedef pair <int, int> pii;
